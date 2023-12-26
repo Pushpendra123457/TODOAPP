d931f3ea3bbd5fb9cae4dbb4f9c486e0c907aff4
pushpendra123457 <pushpendra09042000@gmail.com>
2023-12-27 00:15:32 +0530

    Add project

---
+++ src/components/TodoInput.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+
+function TodoInput(props) {
+    const [inputText,setInputText] = useState('');
+    const handelEnterPress = (e)=>{
+      if(e.keyCode === 13){
+        props.addList(inputText)
+        setInputText('')
+      }
+    }
+  return (
+    <div className='input-container'>
+        <input type = "text" 
+        className='input-box-todo' 
+        placeholder = 'Enter your text'
+        value={ inputText }
+        onChange={e=>{
+            setInputText(e.target.value)
+        }}
+        onKeyDown = { handelEnterPress }
+        />
+        <button className='add-btn'
+        onClick={()=>{
+            props.addList(inputText)
+            setInputText('')
+
+        }}>+</button>
+    </div>
+  )
+}
+
+export default TodoInput
